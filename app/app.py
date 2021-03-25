ad97e74b43074fffdb95abf29f06ca1c8fdc2b5b
Nikita Gudkov <nikitaster2001@gmail.com>
2021-03-25 23:46:30 +0300

    added custom sql queries for assign method

--- app/app.py
+++ app/app.py
@@ -1,22 +1,25 @@
 import os
 
+from typing import List, Dict
+from datetime import datetime
+import json
+
 from fastapi import FastAPI, Response, status, Request
 from fastapi.responses import JSONResponse
 
 import databases
 
-from sqlalchemy import MetaData, create_engine, Table, Column, Integer, String, ForeignKey, Float, ARRAY, TIMESTAMP
-# from sqlalchemy.orm import relationship, mapper, session
+from sqlalchemy import MetaData, create_engine, Table, Column, Integer, \
+    String, ForeignKey, Float, ARRAY, TIMESTAMP, Time, Interval
+
 from sqlalchemy.ext.declarative import declarative_base
 
 from pydantic import BaseModel, Field, ValidationError
-from typing import List, Dict
-from datetime import datetime
-import json
+
 
 DATABASE_URL = "postgresql://{}:{}@db:5432/{}".format(
-    os.getenv('POSTGRES_USER'), 
-    os.getenv('POSTGRES_PASSWORD'), 
+    os.getenv('POSTGRES_USER'),
+    os.getenv('POSTGRES_PASSWORD'),
     os.getenv('POSTGRES_DB'),
 )
 
@@ -26,7 +29,7 @@ database = databases.Database(DATABASE_URL)
 metadata = MetaData()
 
 couriers_types_model = Table(
-    'couriers_types', 
+    'couriers_types',
     metadata,
     Column('id', Integer, primary_key=True),
     Column('name', String(16), nullable=False),
@@ -37,7 +40,6 @@ couriers_types_model = Table(
 couriers_model = Table(
     'couriers',
     metadata,
-
     Column('courier_id', Integer, primary_key=True),
     Column('courier_type', Integer, ForeignKey('couriers_types.id'), nullable=False),
     # Column('courier_type', String, nullable=False),
@@ -54,7 +56,6 @@ couriers_model = Table(
 orders_model = Table(
     'orders',
     metadata,
-    
     Column('order_id', Integer, primary_key=True),
     Column('courier_id', Integer, ForeignKey('couriers.courier_id'), nullable=True),
     Column('weight', Float, nullable=False),
@@ -64,6 +65,24 @@ orders_model = Table(
     Column('completed_at', TIMESTAMP, nullable=True),
 
     # courier = relationship('Couriers', back_populates='orders')
+)
+
+couriers_intervals_model = Table(
+    'couriers_intervals',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('courier_id', Integer, ForeignKey('couriers.courier_id')),
+    Column('time_from', Time),
+    Column('time_to', Time),
+)
+
+orders_intervals_model = Table(
+    'orders_intervals',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('order_id', Integer, ForeignKey('orders.order_id')),
+    Column('time_from', Time),
+    Column('time_to', Time),
 )
 
 engine = create_engine(DATABASE_URL)
@@ -76,6 +95,9 @@ class Courier(BaseModel):
     working_hours: List[str]
     rating: float = None
     earnings: float = None
+
+class CourierAssign(BaseModel):
+    courier_id: int
 
 class CouriersList(BaseModel):
     list_couriers: List[Courier] = Field(..., alias='data')
@@ -105,15 +127,6 @@ async def shutdown():
 
 @app.get("/")
 async def root():
-    # query = notes.insert().values(text='123', completed=True)
-    # last_record_id = await database.execute(query)
-    query = couriers_model.select().where(couriers_model.c.courier_id == 10)
-    # query = couriers_types_model.select().where(couriers_types_model.c.name == 'foot')
-    a = await database.fetch_all(query)
-    if a:
-        return 1
-    else:
-        return 0
     return {"message": "Hello World"}
 
 async def get_couriers_types_dict():
@@ -126,21 +139,30 @@ async def get_couriers_types_dict():
         print(e.args)
     return couriers_types_dict
 
+def get_intervals_from_hours_line(hours_list: List[str], foreign_field_name: str, foreign_field_value: str):
+    result = []
+    for hours_line in hours_list:
+        times = hours_line.split('-')
+        result.append({foreign_field_name: foreign_field_value, 'time_from': datetime.strptime(times[0], '%H:%M').time(), 'time_to': datetime.strptime(times[1], '%H:%M').time()})
+    return result
+
 @app.post("/couriers")
 async def couriers_create(request: Request) -> JSONResponse:
     data = await request.json()
 
     # сделать словарик, чтобы потом подставить значения ключа из таблицы couriers_types
     couriers_types_dict = await get_couriers_types_dict()
-    
+
     query_values = []
+    couriers_intervals = []
     successful = []
-    
+
     try:
         couriers = CouriersList.parse_obj(data)
         for courier in couriers.list_couriers:
             courier.courier_type = couriers_types_dict[str(courier.courier_type)]
             query_values.append(courier.dict())
+            couriers_intervals += get_intervals_from_hours_line(courier.working_hours, 'courier_id', courier.courier_id)
             successful.append({'id': courier.courier_id})
 
     except ValidationError as errs:
@@ -156,6 +178,8 @@ async def couriers_create(request: Request) -> JSONResponse:
 
     try:
         await database.execute_many(couriers_model.insert(), values=query_values)
+        # заполним табличку с временными метками
+        await database.execute_many(couriers_intervals_model.insert(), values=couriers_intervals)
         return JSONResponse(status_code=status.HTTP_201_CREATED, content={'couriers':successful})
     except Exception as errs:
         # есть гарантия, что courier_id всегда уникальны
@@ -185,6 +209,11 @@ async def couriers_edit(courier_id: int, request: Request) -> JSONResponse:
                 courier.regions = request_json['regions']
             if 'working_hours' in request_json.keys():
                 courier.working_hours = request_json['working_hours']
+                couriers_intervals = get_intervals_from_hours_line(courier.working_hours, 'courier_id', courier.courier_id)
+                # удалить старые интревалы
+                await database.execute(couriers_intervals_model.delete().where(couriers_intervals_model.c.courier_id == courier_id))
+                # сохранить новые
+                await database.execute_many(couriers_intervals_model.insert(), values=couriers_intervals)
             if 'courier_type' in request_json.keys():
                 courier.courier_type = request_json['courier_type']
 
@@ -204,9 +233,12 @@ async def couriers_edit(courier_id: int, request: Request) -> JSONResponse:
 async def orders_create(request: Request) -> JSONResponse:
     data = await request.json()
 
+    orders_intervals_queries = []
+
     try:
         orders = OrdersList.parse_obj(data)
-        # return orders
+        for order in orders.list_orders: 
+            orders_intervals_queries += get_intervals_from_hours_line(order.delivery_hours, 'order_id', order.order_id)
 
     except ValidationError as errs:
         errs = json.loads(errs.json())
@@ -220,7 +252,67 @@ async def orders_create(request: Request) -> JSONResponse:
         successful = [{'id': order.order_id} for order in orders.list_orders]
         queries = [order.dict() for order in orders.list_orders]
         await database.execute_many(orders_model.insert(), values=queries)
+        # сохраним в бд временные метки (если доабвление заказов вызовет исключение, не выполнится)
+        await database.execute_many(orders_intervals_model.insert(), values=orders_intervals_queries)
         return JSONResponse(status_code=status.HTTP_201_CREATED, content={'orders':successful})
     except Exception as errs:
         # есть гарантия, что order_id всегда уникальны
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=errs.args)
+
+
+@app.post('/orders/assign')
+async def order_assing(courier: CourierAssign) -> JSONResponse:
+    # проверим, есть ли курьер c таким courier_id
+    try:
+        record = await database.fetch_one(couriers_model.select().where(couriers_model.c.courier_id == courier.courier_id))
+        if not record:
+            return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"errs:": {"courier_id": courier.courier_id, "msg": "Not found"}})
+        courier = Courier.parse_obj(record)
+    except Exception:
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    
+    # получим количество свободного места у курьера
+    query = """
+        select weight - sum as free_space from
+        (SELECT sum(o.weight), ct.weight FROM orders o
+            inner join couriers c on c.courier_id = :courier_id_value
+            inner join couriers_types ct on c.courier_type = ct.id
+            where o.courier_id = c.courier_id AND o.assign_time IS NOT NULL AND o.completed_at is NULL
+            group by ct.weight
+            ) SUMQUERY
+    """
+
+    free_space = await(database.fetch_val(query, values={'courier_id_value': courier.courier_id}))
+
+
+    # макс. кол-во заказов: выбока по весу, району и графику работы. 
+    # сортируем по весу, чтобы взять больше. completed_at, assign_time должны быть NULL
+    query = """
+        SELECT o.order_id, o.weight FROM orders o
+            inner join orders_intervals oi on oi.order_id = o.order_id
+            inner join couriers_intervals ci on ci.courier_id = :courier_id_value
+            inner join couriers c on c.courier_id = :courier_id_value
+            where o.assign_time is NULL AND o.region = ANY(c.regions)
+            AND oi.time_from >= ci.time_from AND oi.time_to <= ci.time_to
+            AND o.weight <= :free_space
+            order by o.weight ASC
+    """
+
+    orders = await(database.fetch_all(query, values={'courier_id_value': courier.courier_id, "free_space": free_space}))
+
+    if orders:
+        assignments = {"orders": [], 'assign_time': datetime.now()}
+        # получен список подходящих заказов. Назначать их курьеру пока есть место
+        for order in orders:
+            if int(order['weight']) <= free_space:
+                assignments['orders'].append({'id': order['order_id']})
+                orders = await(database.execute(orders_model.update().where(orders_model.c.order_id == order['order_id']).values(courier_id=courier.courier_id, assign_time=assignments['assign_time'])))
+                free_space -= int(order['weight'])
+            else:
+                break
+            
+        return assignments
+    return {}
+
+    
