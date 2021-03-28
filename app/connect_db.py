7177851bc0384c27c50553bc439a9259a2a478dd
Nikita Gudkov <nikitaster2001@gmail.com>
2021-03-29 00:13:40 +0300

    added 'price' column to sqlalchemy table

--- app/connect_db.py
+++ app/connect_db.py
@@ -17,6 +17,7 @@ couriers_types_model = Table(
     Column('id', Integer, primary_key=True),
     Column('name', String(16), nullable=False),
     Column('weight', Float, nullable=False),
+    Column('price', Integer, nullable=False),
 )
 
 couriers_model = Table(
