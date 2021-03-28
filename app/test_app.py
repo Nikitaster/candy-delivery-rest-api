8f06a079e77b90026517f74a416281a02ef70795
Nikita Gudkov <nikitaster2001@gmail.com>
2021-03-28 20:43:29 +0300

    fix db connect

--- app/test_app.py
+++ app/test_app.py
@@ -1,26 +1,43 @@
 import pytest
 from httpx import AsyncClient
+from fastapi.testclient import TestClient
 
-from app import app, startup
+from app import app, database
 
 from test_queries import couriers_create_test_json
 import json
 
+# from connect_db import database
+import databases
 
-@pytest.mark.asyncio
-async def test_root():
-    await startup()
-    async with AsyncClient(app=app, base_url="http://0.0.0.0:8080") as ac:
-        response = await ac.get("/")
+# DATABASE_URL = "postgresql://postgres:postgres@db:5432/test"
+
+client = TestClient(app)
+
+# database = databases.Database('postgresql://postgres:postgres@db:5432/')
+
+
+@pytest.fixture()
+async def setup_function(function):
+    await database.connect()
+
+
+@pytest.fixture()
+async def teardown_function(function):
+    await database.disconnect()
+
+
+def test_a():
+    for i in range(1, len(couriers_create_test_json['requests'])):
+        response = client.post("/couriers",
+                               data=json.dumps(couriers_create_test_json['requests'][i]))
+        assert response.status_code == couriers_create_test_json['responses'][i]['status_code']
+        assert response.json() == couriers_create_test_json['responses'][i]['json']
+
+
+def test_root():
+    response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
 
-@pytest.mark.asyncio
-async def test_couriers_create():
-    async with AsyncClient(app=app, base_url="http://0.0.0.0:8080/") as ac:
-        for i in range(len(couriers_create_test_json['requests'])):
-            response = await ac.post("/couriers",
-                                     data=json.dumps(couriers_create_test_json['requests'][i]))
-            assert response.status_code == couriers_create_test_json['responses'][i]['status_code']
-            assert response.json() == couriers_create_test_json['responses'][i]['json']
